9b80dea17304373e48271ba60b0547dad9b230c2
Ro St <rolandstenger95@gmail.com>
2022-05-21 17:54:37 +0200

    updated

--- train.py
+++ train.py
@@ -75,11 +75,11 @@ if __name__=='__main__':
             if i % 2 == 0:
                 try:
                     batch = next(val_dataloader_iter)
-                    X_val, y_val = batch['X'], batch['y']
+                    X_val, y_val = batch['X'], batch['Y']
                 except StopIteration:
                     val_loader_iter = iter(val_dataloader)
                     batch = next(val_loader_iter)
-                    X_val, y_val = batch['X'], batch['y']
+                    X_val, y_val = batch['X'], batch['Y']
                 X_val = X_val.to(0)
                 y_val = y_val.to(0)
 

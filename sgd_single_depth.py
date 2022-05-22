85f3a79a119624e9d39629c0bae1b7d471b3ba29
Ro St <rolandstenger95@gmail.com>
2022-05-22 13:18:04 +0200

    updated

---
+++ sgd_single_depth.py
@@ -0,0 +1,45 @@
+#! python
+
+#$ -N kedro
+
+# execute from current directory
+#$ -cwd
+
+# Preserve environment variables
+#$ -V
+
+# Provide path to python executable
+#$ -S /home/stenger/smaxxhome/anaconda3/envs/pydiver/bin/python
+
+# Merge error and out
+#$ -j yes
+
+# serial queue
+#$ -q taranis-gpu1.q
+# -q teutates.q
+# -q grannus.q
+
+# Path for output
+#$ -o /data.bmp/heart/DataAnalysis/2020_3DExMedSurfaceToDepth/FromSurface2DepthFinal/logs
+
+# job array of length 1
+# -t 1:2
+# %%
+
+import os
+import numpy as np
+
+# %%
+def main():
+    print(os.environ['SGE_TASK_ID'])
+    SGE_TASK_ID = int(os.environ['SGE_TASK_ID']) - 1
+
+    depth = SGE_TASK_ID
+    time_steps = 32
+
+    os.system(f"python train.py")
+    #os.system(f"python train.py --depth {depth} --time_steps {time_steps}")
+
+
+if __name__ == '__main__':
+    main()
